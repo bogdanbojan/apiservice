91833d7257298a6f49888d1f48127fe7342ee3ea
bogdanbojan <bogdanbojan03@gmail.com>
2022-02-15 12:54:05 +0200

    updated interface name.

--- driver/process.go
+++ driver/process.go
@@ -9,7 +9,7 @@ import (
 // TODO: maybe don't completely shut down app using log.Fatal..
 const url = "https://randomapi.com/api/6de6abfedb24f889e0b5f675edc50deb?fmt=raw&sole"
 
-type RecordConfig interface {
+type RecordConfiger interface {
 	RecordsConfig() (*input.Configuration, error)
 }
 
@@ -26,7 +26,7 @@ type RecordWriter interface {
 }
 
 // Process aggregates the steps that the service has to do in order to transform the data from the API call.
-func Process(rc RecordConfig, rr RecordReader, rt RecordTransformer, rw RecordWriter) {
+func Process(rc RecordConfiger, rr RecordReader, rt RecordTransformer, rw RecordWriter) {
 	config, err := rc.RecordsConfig()
 	filePath := config.FilePath
 	if err != nil {
