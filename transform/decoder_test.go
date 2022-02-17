61fcd1b4ed8b884dd85a0a4f1a83737793afa24a
bogdanbojan <bogdanbojan03@gmail.com>
2022-02-17 12:18:12 +0200

    added TestRemoveDuplicates

--- transform/decoder_test.go
+++ transform/decoder_test.go
@@ -7,8 +7,31 @@ import (
 )
 
 func TestRemoveDuplicates(t *testing.T) {
-	t.Run("testing multiple entries", func(t *testing.T) {
-		records := []read.Record{
+	for _, dc := range duplicatesCases {
+		t.Run(dc.title, func(t *testing.T) {
+			got := removeDuplicates(dc.records)
+			want := dc.want
+			assertDuplicates(t, got, want)
+		})
+	}
+
+}
+
+func assertDuplicates(t testing.TB, got, want []read.Record) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+var duplicatesCases = []struct {
+	title   string
+	records []read.Record
+	want    []read.Record
+}{
+	{
+		title: "multiple entries with one duplicate",
+		records: []read.Record{
 			{
 				FirstName: "Thad",
 				LastName:  "Feest",
@@ -17,20 +40,21 @@ func TestRemoveDuplicates(t *testing.T) {
 				Created:   "July 13, 2021",
 				Balance:   "$1,950.71",
 			},
-			{FirstName: "Thad",
-				LastName: "Feest",
-				Email:    "Thad.Feest@cleve.name",
-				Address:  "77881 Schaefer Loaf",
-				Created:  "July 13, 2021",
-				Balance:  "$1,950.71",
-			},
 			{
 				FirstName: "Thad",
-				LastName:  "Adam",
+				LastName:  "Feest",
 				Email:     "Thad.Feest@cleve.name",
 				Address:   "77881 Schaefer Loaf",
 				Created:   "July 13, 2021",
-				Balance:   "$1,950.71"},
+				Balance:   "$1,950.71",
+			},
+			{
+				FirstName: "Justice",
+				LastName:  "Keebler",
+				Email:     "magentarabbit07@gmail.com",
+				Address:   "173 Hyatt Crossroad",
+				Created:   "April 12, 2014",
+				Balance:   "$1,359.61"},
 			{
 				FirstName: "Sabrina",
 				LastName:  "Kuphal",
@@ -38,23 +62,23 @@ func TestRemoveDuplicates(t *testing.T) {
 				Address:   "03491 Howard Vista",
 				Created:   "August 29, 2018",
 				Balance:   "$6,996.45"},
-		}
-		got := removeDuplicates(records)
-		want := []read.Record{
-			{FirstName: "Thad",
-				LastName: "Feest",
-				Email:    "Thad.Feest@cleve.name",
-				Address:  "77881 Schaefer Loaf",
-				Created:  "July 13, 2021",
-				Balance:  "$1,950.71",
-			},
+		},
+		want: []read.Record{
 			{
 				FirstName: "Thad",
-				LastName:  "Adam",
+				LastName:  "Feest",
 				Email:     "Thad.Feest@cleve.name",
 				Address:   "77881 Schaefer Loaf",
 				Created:   "July 13, 2021",
-				Balance:   "$1,950.71"},
+				Balance:   "$1,950.71",
+			},
+			{
+				FirstName: "Justice",
+				LastName:  "Keebler",
+				Email:     "magentarabbit07@gmail.com",
+				Address:   "173 Hyatt Crossroad",
+				Created:   "April 12, 2014",
+				Balance:   "$1,359.61"},
 			{
 				FirstName: "Sabrina",
 				LastName:  "Kuphal",
@@ -62,14 +86,37 @@ func TestRemoveDuplicates(t *testing.T) {
 				Address:   "03491 Howard Vista",
 				Created:   "August 29, 2018",
 				Balance:   "$6,996.45"},
-		}
-		assertDuplicates(t, got, want)
-	})
-}
-
-func assertDuplicates(t testing.TB, got, want []read.Record) {
-	t.Helper()
-	if !reflect.DeepEqual(got, want) {
-		t.Errorf("got %q, want %q", got, want)
-	}
+		},
+	},
+	{
+		title: "all duplicate entries",
+		records: []read.Record{
+			{
+				FirstName: "Thad",
+				LastName:  "Feest",
+				Email:     "Thad.Feest@cleve.name",
+				Address:   "77881 Schaefer Loaf",
+				Created:   "July 13, 2021",
+				Balance:   "$1,950.71",
+			},
+			{
+				FirstName: "Thad",
+				LastName:  "Feest",
+				Email:     "Thad.Feest@cleve.name",
+				Address:   "77881 Schaefer Loaf",
+				Created:   "July 13, 2021",
+				Balance:   "$1,950.71",
+			},
+		},
+		want: []read.Record{
+			{
+				FirstName: "Thad",
+				LastName:  "Feest",
+				Email:     "Thad.Feest@cleve.name",
+				Address:   "77881 Schaefer Loaf",
+				Created:   "July 13, 2021",
+				Balance:   "$1,950.71",
+			},
+		},
+	},
 }
