309170d99489f131b65106a91906f112f21c8311
bogdanbojan <bogdanbojan03@gmail.com>
2022-03-03 18:56:33 +0200

    split up WriteRecords into multiple functions.

--- write/writer.go
+++ write/writer.go
@@ -19,11 +19,27 @@ func NewFileWriter() *FileWriter {
 // WriteRecords exports the records after they have been filtered, formatted and grouped into
 // a JSON in the current user folder.
 func (fw *FileWriter) WriteRecords(records []transform.ExportRecords, filePath string) error {
+	file, err := marshalRecords(records)
+	if err != nil {
+		return err
+	}
+	err = writeJSON(file, filePath)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func marshalRecords(records []transform.ExportRecords) ([]byte, error) {
 	file, err := json.MarshalIndent(records, " ", "\t")
 	if err != nil {
-		return fmt.Errorf("cannot marshal json: %w", err)
+		return nil, fmt.Errorf("cannot marshal json: %w", err)
 	}
-	err = ioutil.WriteFile(filePath, file, 0644)
+	return file, nil
+}
+
+func writeJSON(file []byte, filePath string) error {
+	err := ioutil.WriteFile(filePath, file, 0644)
 	if err != nil {
 		return fmt.Errorf("cannot write json: %w", err)
 	}
